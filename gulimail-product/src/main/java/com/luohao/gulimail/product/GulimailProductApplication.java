3ed3f0c5c23bbe23207ba130be540655614376e7
luohao <18779121256@163.com>
2023-11-14 14:31:13 +0800

    快速开发，逆向工程的搭建配置

--- gulimail-product/src/main/java/com/luohao/gulimail/product/GulimailProductApplication.java
+++ gulimail-product/src/main/java/com/luohao/gulimail/product/GulimailProductApplication.java
@@ -3,6 +3,18 @@ package com.luohao.gulimail.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* 整合mybatisplus
+*    <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-web</artifactId>
+    </dependency>
+    *
+    * 配置数据源
+    *   数据库驱动
+    *   配置数据源
+    * 配置mybatisplus
+* */
 @SpringBootApplication
 public class GulimailProductApplication {
 
--- gulimail-product/src/test/java/com/luohao/gulimail/product/GulimailProductApplicationTests.java
+++ gulimail-product/src/test/java/com/luohao/gulimail/product/GulimailProductApplicationTests.java
@@ -1,13 +1,26 @@
 package com.luohao.gulimail.product;
 
+import com.luohao.gulimail.product.entity.BrandEntity;
+import com.luohao.gulimail.product.service.BrandService;
+//import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes =GulimailProductApplication.class)
 class GulimailProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
